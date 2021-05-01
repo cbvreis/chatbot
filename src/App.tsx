cba1f04337c4f03c414947649533fd0283df8e29
carlos-mattos <ceamattos.jf@gmail.com>
2021-05-01 17:27:40 -0300

    primeira versao teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import api from './services/api';
+
+interface IMessages {
+  from: string;
+  content: string | undefined;
+}
 
 function App() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const [messages, setMessages] = useState<IMessages[]>([{} as IMessages]);
+  const [isRobotTurn, setIsRobotTurn] = useState(false);
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    setMessages([
+      ...messages,
+      { from: 'me', content: inputRef.current?.value },
+    ]);
+
+    setIsRobotTurn(true);
+  }
+
+  useEffect(() => {
+    if (isRobotTurn) {
+      api
+        .get(`get?msg=${inputRef.current?.value}`)
+        .then((response) =>
+          setMessages([
+            ...messages,
+            { from: 'robot', content: response.data.irineu_bot },
+          ])
+        );
+    }
+
+    setIsRobotTurn(false);
+  }, [isRobotTurn]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={handleSubmit}>
+        <input
+          name='message'
+          placeholder='Digite aqui...'
+          type='text'
+          ref={inputRef}
+        />
+      </form>
+
+      <ul>
+        {messages &&
+          messages.map((message, index) => {
+            if (index === 0) {
+              return <div key={index}></div>;
+            } else {
+              return (
+                <li key={index}>
+                  {message.from}: {message.content}
+                </li>
+              );
+            }
+          })}
+      </ul>
     </div>
   );
 }
